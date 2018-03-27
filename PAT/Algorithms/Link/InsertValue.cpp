d8cf55539961af07dcfd2aebc713b64136e04138
LI-RENXIANG\lrx0014 <lrx0014@hotmail.com>
2018-03-27 15:36:30 +0800

    Fix InserValue of ListLink

--- PAT/Algorithms/Link/InsertValue.cpp
+++ PAT/Algorithms/Link/InsertValue.cpp
@@ -4,64 +4,74 @@
 
 using namespace std;
 
-struct ListNode {
+struct ListNode
+{
     int val;
     struct ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-class InsertValue {
-public:
-    ListNode* insert(vector<int> A, int val) {
-        // write code here
-        ListNode* insert_node = new ListNode(val);
-        if(A.size()==0)
-        {
-            insert_node->next = insert_node;
-            return insert_node;
-        }
-         
-        ListNode* head = new ListNode(A[0]);
-        ListNode* cur = head;
-        for(int i=1;i<A.size();i++)
-        {
-            ListNode* temp = new ListNode(A[i]);
-            cur->next = temp;
-            cur = temp;
-        }
-        cur->next = head;
-         
-        ListNode* visit = head;
-        if(visit->val < insert_node->val)
-        {
-            ListNode* pre = visit;
-            visit = visit->next;
-            while(visit!=head)
-            {
-                if(visit->val < insert_node->val)
-                {
-                    pre = visit;
-                    visit = visit->next;
-                }else{
-                    pre->next = insert_node;
-                    insert_node->next = visit;
-                    return head;
-                }
-            }
-            pre->next = insert_node;
-            insert_node->next = NULL;
-            return head;
-        }else{
-            ListNode* pre = visit;
-            visit = visit->next;
-            while(visit!=head)
-            {
-                pre = visit;
-                visit = visit->next;
-            }
-            pre->next = insert_node;
-            insert_node->next = NULL;
-        }
-        return head;
+ListNode* Insert(vector<int> A, vector<int> nxt, int val)
+{
+    if(A.empty() || nxt.empty())
+    {
+        return NULL;
     }
-};
+    ListNode* head = new ListNode(A[0]);
+    ListNode* temp = head;
+    for(int i=0;i<A.size()-1;i++)
+    {
+        ListNode* newNode = new ListNode(A[nxt[i]]);
+        temp->next = newNode;
+        temp = newNode;
+    }
+    ListNode* node_in = new ListNode(val);
+    if(val < head->val)
+    {
+        temp->next = node_in;
+        node_in->next = head;
+        return node_in;
+    }
+    ListNode* pre = head;
+    ListNode* cur = pre->next;
+    while(cur != NULL)
+    {
+        if(pre->val <= val && val <= cur->val)
+        {
+            break;
+        }
+        pre = cur;
+        cur = cur->next;
+    }
+    pre->next = node_in;
+    node_in->next = cur;
+    if(val < head->val)
+    {
+        return node_in;
+    }
+    return head;
+}
+
+void showLink(ListNode* head)
+{
+    if(head == NULL)
+    {
+        return;
+    }
+    while(head != NULL)
+    {
+        printf("%d ",head->val);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int a[] = {1,3,4,5,7};
+    int b[] = {1,2,3,4,0};
+    vector<int> A(begin(a),end(a));
+    vector<int> nxt(begin(b),end(b));
+    ListNode* head = Insert(A,nxt,2);
+    showLink(head);
+}
